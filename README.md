deda9beba96dc14c496e8d3c7534c24f6353a519
0x0a0d <vandug23@gmail.com>
2021-09-28 16:15:28 +0700

    support typescript

--- README.md
+++ README.md
@@ -1,10 +1,17 @@
-### getLatestChromeVersion([platform=win|mac])
+# latest-chrome-version
+
+Get latest chrome version
 
 > npm i latest-chrome-version
 
+### Usage
+- latestChromeVersion([platform = 'win' | 'mac'])
+
 ```js
-const getLatestChromeVersion = require('latest-chrome-version')
-getLatestChromeVersion().then(console.log).catch(console.error) // 81.0.4044.138
+import { latestChromeVersion } from 'latest-chrome-version'
+// const { latestChromeVersion } = require('latest-chrome-version')
+
+latestChromeVersion().then(console.log).catch(console.error) // 81.0.4044.138
 ```
 
 ### Author
---
+++ index.ts
@@ -0,0 +1,40 @@
+/**
+ * parse response version
+ */
+import { v4 } from 'uuid'
+import axios from 'axios'
+
+const parseVersion = (xml: string): string => {
+  const m = xml.match(/<manifest version="([^"]+)"/)
+  if (m == null) throw new Error('parseVersion failed')
+  return m[1]
+}
+
+type Platform = 'win' | 'mac'
+const buildRequestData = (platform: Platform, os_version: string, app_id: string) => {
+  return `<request protocol="3.0" version="KeystoneAdmin-1.2.5.1190" ismachine="0" requestid="{${v4()}}" dedup="cr" sessionid="{${v4()}}" installsource="ondemandupdate">
+        <os platform="${platform}" version="${os_version}" arch="x64" sp="" />
+        <app appid="${app_id}" version="71">
+                <updatecheck />
+        </app>
+</request>`
+}
+
+/**
+ * get latest chrome version
+ */
+export const latestChromeVersion = (platform: 'mac' | 'win'): Promise<string> => {
+  const requestData = platform === 'win'
+  ? buildRequestData('win', '10.0.18363.0', '{8A69D345-D564-463C-AFF1-A69D9E530F96}')
+    : buildRequestData('mac', '10.12', 'com.google.Chrome')
+
+  return axios.post('https://tools.google.com/service/update2', requestData, {
+    params: {'cup2hreq': 'foo', 'cup2key': 'bar'},
+    headers: {
+      'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.0.0 Safari/537.36'
+    },
+  }).then(res => {
+    if (res.status !== 200) throw new Error('Request failed. Response status should be 200')
+    return parseVersion(res.data)
+  })
+}
